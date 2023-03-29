dc0ae20a3c71a57604dfbb493af9d9ad72de0997
Gorgud079 <des079@inbox.ru>
2023-03-29 14:03:54 +0400

    added media_root, added Woman model

--- website3/settings.py
+++ website3/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-s$9xr05vn)4sj*-0+q9^161-6ycsfs0koh5i%&ir69=8nffs#k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'woman.apps.WomanConfig',
 ]
 
 MIDDLEWARE = [
@@ -116,6 +118,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ woman/urls.py
@@ -0,0 +1,8 @@
+from .views import *
+from django.urls import path, re_path
+
+urlpatterns = [
+    path('', index, name='home'),
+    path('woman/<slug:cat>/', categories),
+    re_path(r'^archive/(?P<year>[0-9]{4})/', archive)
+]
